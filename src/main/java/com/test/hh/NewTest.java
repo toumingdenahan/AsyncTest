c1a1f00cb9c6ee6c9e551c295546b1f250098b76
xnsxl <shixiaolin9@jd.com>
2020-03-16 16:33:27 +0800

    测试合并分支冲突

--- src/main/java/com/test/hh/NewTest.java
+++ src/main/java/com/test/hh/NewTest.java
@@ -4,5 +4,8 @@ public class NewTest {
 
     public static void main(String[] args) {
         System.out.println("测试合并分支");
+        System.out.println("测试合并分支");
+        System.out.println("测试合并分支");
+
     }
 }
