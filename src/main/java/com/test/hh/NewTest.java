8e0ca9346feba317ab81f5e75463c6cb347e2a2e
xnsxl <shixiaolin9@jd.com>
2020-03-16 16:59:21 +0800

    回退已经提交的代码

--- src/main/java/com/test/hh/NewTest.java
+++ src/main/java/com/test/hh/NewTest.java
@@ -4,8 +4,7 @@ public class NewTest {
 
     public static void main(String[] args) {
         System.out.println("测试合并分支");
-        System.out.println("测试合并分支");
-        System.out.println("测试合并分支");
 
+        System.out.println("测试回退远程代码");
     }
 }
